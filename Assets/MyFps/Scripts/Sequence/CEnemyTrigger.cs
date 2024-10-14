99a4015a3bd6484ea16d216f869f3064c00fa517
bokki04-admin <anvill1234@gmail.com>
2024-10-14 13:10:28 +0900

    1. 적(로봇) 애니메이션 리소스 추가 및 애니메이터 구현 2. 적 트리거 구현 3. 사운드(데미지효과, 문열기, 적 출현), 이미지(데미지 효과, 머즐효과) 추가

--- Assets/MyFps/Scripts/Sequence/CEnemyTrigger.cs
+++ Assets/MyFps/Scripts/Sequence/CEnemyTrigger.cs
@@ -8,9 +8,13 @@ namespace MyFps
     public class CEnemyTrigger : MonoBehaviour
     {
         #region Variables
-        public GameObject theDoor;
+        public GameObject theDoor;          //문
 
-        public GameObject thePlayer;
+        public AudioSource doorBang;        //문 열기 사운드
+        public AudioSource JumpscareTune;   //적 등장 사운드
+
+        public GameObject thePlayer;        //플레이어
+        public GameObject theRobot;         //로봇
 
         #endregion
         // Start is called before the first frame update
@@ -37,7 +41,16 @@ namespace MyFps
 
             yield return new WaitForSeconds(1f);
 
+            //적 소환
+            theRobot.SetActive(true);
+
             theDoor.GetComponent<Animator>().SetBool("IsOpen", true);
+
+            //문 사운드
+            doorBang.Play();
+            
+            //적 등장 사운드
+            JumpscareTune.Play();
             
 
             yield return new WaitForSeconds(1f);
